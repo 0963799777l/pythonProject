727a6ddf9e749bed43a38affcbd8aa6f4b354e3a
0963799777l <shatova8888@gmail.com>
2022-05-23 18:36:25 +0300

    Initial commit

---
+++ lesson3.py
@@ -0,0 +1,6 @@
+appel = int(input("Кількість яблук:"))
+students = int(input("Кількість учнів:"))
+
+
+print(("Кількість яблук на учня:"),int(appel // students))
+print(("Кількість яблук на учня:"),int(appel % students))
---
+++ main.py
@@ -0,0 +1,11 @@
+students_1_klas = int(input("Кількість учнів:"))
+students_2_klas = int(input("Кількість учнів:"))
+students_3_klas = int(input("Кількість учнів:"))
+
+
+print(("Кількість парт:"),int(students_1_klas + students_2_klas + students_3_klas // 2))
+print(("Кількість парт:"),int(students_1_klas + students_2_klas + students_3_klas % 2))
+
+
+
+
